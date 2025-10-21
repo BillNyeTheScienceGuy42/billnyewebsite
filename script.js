129fde01c7daafee1d1ab2ecc59bdf605548b9d9
lucidmonke <127571359+lucidmonke@users.noreply.github.com>
2025-10-21 20:31:58 +0100

    ??

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-    const bigLogo = document.querySelector('.big-logo');
+    const logo = document.querySelector('.logo');
     const monkeySound = document.getElementById('monkey-sound');
-    bigLogo.addEventListener('click', () => {
-        bigLogo.classList.add('shake');
+    logo.addEventListener('click', () => {
+        logo.classList.add('shake');
         monkeySound.currentTime = 0;
         monkeySound.play();
         setTimeout(() => bigLogo.classList.remove('shake'), 500);
