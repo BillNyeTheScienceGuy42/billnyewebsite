2375c6359b5fbd05a389d53dbd343950fe49bc96
lucidmonke <127571359+lucidmonke@users.noreply.github.com>
2025-10-21 20:58:57 +0100

    remove scripts

--- script.js
+++
@@ -1,9 +0,0 @@
-
-    const logo = document.querySelector('.logo');
-    const monkeySound = document.getElementById('monkey-sound');
-    logo.addEventListener('click', () => {
-        logo.classList.add('shake');
-        monkeySound.currentTime = 0;
-        monkeySound.play();
-        setTimeout(() => bigLogo.classList.remove('shake'), 500);
-    });
